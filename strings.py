22a9ce6f95e7c239710d3819637a49f2223dc364
Bruno Tsurushima <btsuuru456@gmail.com>
2017-08-28 16:42:44 -0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf=8 -*-
+
+# Jogo da batata quente
+
+quente = "quente"
+for i in range(1,11):
+  print "Batata ", quente*i, " "
+  print "Queimou!"
+print "fim de jogo!"
+
+palavra = "Bruno "
+print palavra*2
+print palavra*5
+print palavra*10
